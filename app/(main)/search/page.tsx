4c20f55a8a91c2ae3abab24145b22280f8c0d1c6
ConnerMorgan <connermo@umich.edu>
2025-12-04 20:04:32 -0500

    minor change

--- app/(main)/search/page.tsx
+++ app/(main)/search/page.tsx
@@ -177,7 +177,7 @@ export default function SearchPage() {
                 className="w-full overflow-x-auto pb-1"
                 style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}
             >
-                <div className="flex gap-2 min-w-max pr-2">
+                <div className="inline-flex gap-2 pr-2">
                     <Chip
                         label="Distance"
                         value={distanceKm ? `${distanceKm} km` : 'Any'}
